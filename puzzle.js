42e5d0f18f2cd8863ddf2891360a74ee4a0e0c3b
Sandu Tricolici <tricolicisandu@gmail.com>
2025-11-22 00:20:03 +0100

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -2,22 +2,30 @@
 var rows = 3;
 var columns = 3;
 
-var currTile;//Refernce the tile we click to switch with the blank tile
+var currentTile;//Refernce the tile we click to switch with the blank tile
 var otherTile; //blank tile
 
 var turns = 0;
 
-// var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
+//this is the order we are placing the images in the puzzle box, we can choose where the peaces are going to be placed 
 
-window.onload = function() {
-    for (let r=0; r < rows; r++) {
-        for (let c=0; c < columns; c++) {
+//var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var imgOrder = ["1", "2", "8", "5", "4", "6", "7", "9", "3"];
 
-            //<img id="0-0" src="1.jpg">
+window.onload = function() //this function makes sure that the page finished loading first and then the function will run 
+{
+    //we are placing the images from the previos Array
+
+    for (let r=0; r < rows; r++) //a for loop for rows
+        {
+        for (let c=0; c < columns; c++) //a for loop for colums
+            {
+            //we are placing for every box a img,
+            //example <img id="0-0" src="1.jpg">
             let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
-            tile.src = imgOrder.shift() + ".jpg";
+            tile.src = "IMG/image_part_00" + imgOrder.shift() + ".jpg";
+
 
             //DRAG FUNCTIONALITY
             tile.addEventListener("dragstart", dragStart);  //click an image to drag
@@ -29,23 +37,29 @@ window.onload = function() {
 
             document.getElementById("board").append(tile);
 
+
         }
     }
 }
 
-function dragStart() {
+function dragStart() 
+{
     currTile = this; //this refers to the img tile being dragged
 }
 
-function dragOver(e) {
+//this function allows browser to dropp the img, and the tile can accept a drop
+function dragOver(e) 
+{
+    e.preventDefault();
+}
+//again a function to allow dropping
+function dragEnter(e) 
+{
     e.preventDefault();
 }
 
-function dragEnter(e) {
-    e.preventDefault();
-}
-
-function dragLeave() {
+function dragLeave() 
+{
 
 }
 
@@ -53,20 +67,26 @@ function dragDrop() {
     otherTile = this; //this refers to the img tile being dropped on
 }
 
-function dragEnd() {
-    if (!otherTile.src.includes("3.jpg")) {
-        return;
-    }
+//the final destination to any dragged image should only be in the empty img
+function dragEnd() 
+{
+    if (!otherTile.src.includes("003.jpg")) 
+        {
+           return;
+        }
 
-    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
-    let r = parseInt(currCoords[0]);
-    let c = parseInt(currCoords[1]);
+        //we are taking the id coordonates from the dragged img, turning into an array 
+    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"], also its removes the "-" between
+    let r = parseInt(currCoords[0]);// takes the first string value and turns into a int index stored in r
+    let c = parseInt(currCoords[1]);//then the second string value
 
+        //same thing here 
     let otherCoords = otherTile.id.split("-");
     let r2 = parseInt(otherCoords[0]);
     let c2 = parseInt(otherCoords[1]);
 
-    let moveLeft = r == r2 && c2 == c-1;
+    //check for adjacency, so we can only move the img to a nearby box
+    let moveLeft = r == r2 && c2 == c-1;//to move left you need to be placed in the same r and -1 for colums
     let moveRight = r == r2 && c2 == c+1;
 
     let moveUp = c == c2 && r2 == r-1;
@@ -74,7 +94,8 @@ function dragEnd() {
 
     let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
 
-    if (isAdjacent) {
+    if (isAdjacent) 
+        {
         let currImg = currTile.src;
         let otherImg = otherTile.src;
 
@@ -83,7 +104,6 @@ function dragEnd() {
 
         turns += 1;
         document.getElementById("turns").innerText = turns;
-    }
-
-
+        }
 }
+
